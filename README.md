dbf5cdf658d5f1a1a181a1077c84e7e9798622eb
peterianmuller <peterianmuller@gmail.com>
2019-04-09 10:15:39 -0700

    Update js file to use local variable to store answerString

---
+++ README.md
@@ -0,0 +1,23 @@
+This repo houses projects for the end of SSP! We'll be diving into how a JavaScript file works with HTML and CSS file to create an interactive web application.
+
+Learning Goals:
+
+How functions can be used to update the DOM elements (HTML elements) displayed on the browser
+How JavaScript can listen for user interaction events, like clicks, and update the page
+How we can use the JavaScript eval function to evaluate javascript as a string. In our case, we'll use eval to evaluate math equations that is currently a string.
+
+Instructions for working on the code on your machine offline:
+
+Download a text editor: I work with Sublime Text: https://www.sublimetext.com/3 However, there are many other cool options: Atom, Visual Studio Code
+
+Downloading the source code from Github:
+
+Sign into your github account
+click on this link to find the repo : https://github.com/peterianmuller/SSP-Projects
+Click the fork button in the upper right hand corner. This will create a copy of this current repository (repo) on your personal github profile
+Navigate to that fork
+Once on the page for your fork - click the green button that says: Clone or Download - Click download ZIP
+open the zip file in a location you will remember on your laptop
+
+Instructions for working on the code online on repl:
+Follow this link: https://repl.it/repls/MistyroseUrbanDrupal
--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-// We are building up a string of operations to evaluate in the text input from the html:
+var answerString = '';
+
+// We are building up a string of operations to evaluate. We'll keep track of the state of the string of operations in the variable answerString. We'll also display the string as it's being built in the following html element:
 
 // <input type="text" id="answer" />
 
-// The idea here is to build up a string of operations from the different buttons of the calculator
-// We'll do this with the addNumberOrOperationToInput function.
+// There are three functions here to finish:
+// addNumberOrOperationToInput builds up the answerString by adding either a number 0-9 or an operation. Note that we concatinate the value of the number and +, /, *, - operator both to the answerString and we want to display the string correctly in the text input HTML element shown above.
 
-// Then we want to evaluate the string of operations using the evaluateValue function.
-// evaluateValue should fire when the the button that displays the = sign is clicked
+// clearVals sets the answerString back to ''. This function should also make the html element with an id of answer display an empty string.
+
+// evaluateValue evaluates the equation the answer string and reassigns the answerString to the result of that operation. It also makesthe html element with an id of answer display the result of the evaluated answerString.
+// hint for evaluateValue function: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
 
 // add each key and operation to the text input
 function addNumberOrOperationToInput(val) {
+	// increment the answerString by val
 	// grab the element that has the id of answer and save it to a variable called answerField
-	// increment the value property of answerField by the argument passed into addNumberOrOperationToInput
+	// increment the value property of answerField by the variable answerString
 }
 
 // clear functionality
 function clearVals() {
+	// reassign the variable answerString to an empty string
 	// grab the element that has the id of answer and save it to a variable called answerField
-	// reassign the value property of answerField to an empty string
+	// reassign the value property of answerField to answerString
 }
 
 // evaulate functionality
 function evaluateValue() {
+	// declare a variable evaluatedAnswer set to the value of passing answerString into the built in eval function
 	// grab the element that has the id of answer and save it to a variable called answerField
-	// declare a variable answer set to the value property of answerField passed to the built-in
-	// eval function as an argument
+	// reassign the value property of answerField to evaluatedAnswer to evaluatedAnswer
 }
