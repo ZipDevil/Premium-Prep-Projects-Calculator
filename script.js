ad1dbc9447fb82618488d6b9f6ca03d2d868b13b
peterianmuller <peterianmuller@gmail.com>
2019-04-09 09:50:59 -0700

    Initial Commit -> Added html,css,and js files. Js files commented out with suggestions

---
+++ script.js
@@ -0,0 +1,28 @@
+// We are building up a string of operations to evaluate in the text input from the html:
+
+// <input type="text" id="answer" />
+
+// The idea here is to build up a string of operations from the different buttons of the calculator
+// We'll do this with the addNumberOrOperationToInput function.
+
+// Then we want to evaluate the string of operations using the evaluateValue function.
+// evaluateValue should fire when the the button that displays the = sign is clicked
+
+// add each key and operation to the text input
+function addNumberOrOperationToInput(val) {
+	// grab the element that has the id of answer and save it to a variable called answerField
+	// increment the value property of answerField by the argument passed into addNumberOrOperationToInput
+}
+
+// clear functionality
+function clearVals() {
+	// grab the element that has the id of answer and save it to a variable called answerField
+	// reassign the value property of answerField to an empty string
+}
+
+// evaulate functionality
+function evaluateValue() {
+	// grab the element that has the id of answer and save it to a variable called answerField
+	// declare a variable answer set to the value property of answerField passed to the built-in
+	// eval function as an argument
+}
